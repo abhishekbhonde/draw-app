37846697495d1177267a093d735f24f6f0679084
abhishekbhonde <abhishekbhonde567@gmail.com>
2025-09-17 00:02:04 +0530

    feat: 🎉 added prisma package and added enpoints in http server

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 const app = express();
 
 app.get("/", (req,res)=>{
@@ -8,4 +9,25 @@ app.get("/", (req,res)=>{
     })
 })
 
+
+app.post("/sign-up", (req,res)=>{
+    //db call
+    return res.json({
+        "message":"User created"
+    })
+})
+
+
+app.post("/sign-in", (req,res)=>{   
+    //db call
+
+    const userId = 1;
+
+    const token = jwt.sign({userId}, JWT_SECRET);
+
+    return res.json({
+        "message":"User signed in",
+        "token": token
+    })
+})
 app.listen(3001);
