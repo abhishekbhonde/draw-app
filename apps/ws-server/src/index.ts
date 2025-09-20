36478cbcc9dbfd925aa9caf9eee3d218a872655a
abhishekbhonde <abhishekbhonde567@gmail.com>
2025-09-20 13:01:49 +0530

    feat: 🎉 add authentication to ws server

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,8 +1,20 @@
 import { WebSocketServer } from 'ws';
-
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config';
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+    const url = request.url;
+    if(!url){
+      return 
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token');
+    const decoded = jwt.verify(token as string, JWT_SECRET)
+    if(!decoded){
+      ws.close();
+      return;
+    }
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
